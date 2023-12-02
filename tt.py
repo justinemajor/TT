524036f786ac6f0272e098b7022858aa81db48b2
Justine Major <jumaj7@ulaval.ca>
2023-12-02 16:14:02 -0500

    arrays of 51 points and 3 temps - bug with iterations after the first one

--- tt.py
+++ tt.py
@@ -19,8 +19,10 @@ Tfin = [-16, 11, 25]  # froid, extérieur, celsius
 Tcin = 215  # chaud in, celsius
 mdotf = 3398  # m3/h, remettre en secondes, débit froid
 mdotc = 10194  # m3/h, remettre en secondes, débit chaud
-tparoi = 0.6*10**-3  # m
+tparoi = 0.6*(10**-3)  # m
 nplaques = np.arange(start=50, stop=305, step=5)  # pour chaque écoulement
+nb = len(nplaques)
+nplaques = np.vstack([nplaques]*3).transpose().reshape((nb, 3, 1))
 l = 1  # m
 w = [1, 0.4]  # m [flux croise, contre-courant]
 # tcanal = np.array([5*10**-3])  # faire varier
@@ -31,8 +33,21 @@ kparoi = 205  # alu, essayer avec cuivre aussi
 tcanal = (2-(2*nplaques-1)*tparoi)/2/nplaques  # si n=300, on a 2.7 mm d'épaisseur de canal
 
 
+# flux à contre-courants
+idx_w = 1
+width = w[idx_w]
+
+Ac = tcanal*width  # l'air chaud circule dans le sens de la longueur pour un transfert de chaleur maximal
+Af = tcanal*width
+Pc = 2*(tcanal+l)
+Pf = 2*(tcanal+l)
+Dhf = 4*Af/Pf
+Dhc = 4*Ac/Pc
+
+
 Tci = 215+273.15
-Tfi = [284.4, 257, 298]
+Tfi = np.array([284.4, 257, 298])
+Tfi = np.vstack([Tfi]*nb).reshape((nb, 3, 1))  # 51 x 3 x 1
 
 #si ajout eau
 #water_cst = Fluid(FluidsList.Water).dew_point_at_temperature(Tci).dew_point_at_pressure(101325).as_dict()
@@ -44,86 +59,78 @@ Tcm = [177, 175, 178] # degrés C
 #Tfm = [93, 81, 99] # degrés C pour flux croise
 #Tfm = [97, 85, 102] # degrés C pour contre-courant
 Tfm = [96, 84, 102] # degrés C moyenne entre les deux configurations
+Tm = Tcm+Tfm
+Tmat = np.array(Tm*nb).reshape([nb, 6, 1])
 
-#nplaques = 200
-#Tcm = 173 # pour flux croise
-#Tfm = [104, 93, 109] # degrés C pour flux croise
-#Tcm = 172  # pour contre-courant
-#Tfm = [109, 97, 114] # degrés C pour contre-courant
-Tcm = 172  # moyen
-Tfm = [106, 95, 111] # degrés C moyenne entre les deux configurations
+qmat = np.array([])
+Emat = np.array([])
 
-humid_air_c = [HumidAir().with_state(
-    InputHumidAir.altitude(80),
-    InputHumidAir.temperature(i),
-    InputHumidAir.relative_humidity(0)).as_dict() for i in Tcm]
-# print('air chaud', humid_air_c)
+for it in range(5):
+    lastf = Tmat[:,3:,-1]  # 51 x 3
+    lastc = Tmat[:,:3, -1]  # 51 x 3
+    tempf = list(lastf.reshape((lastf.size)))
+    tempc = list(lastc.reshape((lastc.size)))
 
-humid_air_f = [HumidAir().with_state(
-    InputHumidAir.altitude(80),
-    InputHumidAir.temperature(i),
-    InputHumidAir.relative_humidity(50)).as_dict() for i in Tfm]
-# print('air froid', humid_air_f)
+    humid_air_c = [HumidAir().with_state(
+        InputHumidAir.altitude(80),
+        InputHumidAir.temperature(i),
+        InputHumidAir.relative_humidity(0)).as_dict() for i in tempc]
+    # print('air chaud', humid_air_c)
 
-
-# constantes pour l'air
-# Tk = np.array([488, tcm2, tcm3, 284.4, 257, 298])
-cp = np.array([i['specific_heat'] for i in (humid_air_c + humid_air_f)])
-# mu = np.array([i['dynamic_viscosity'] for i in (humid_air_c + humid_air_f)])
-k = np.array([i['conductivity'] for i in (humid_air_c + humid_air_f)])
-# Pr = np.array([i['prandtl'] for i in (humid_air_c + humid_air_f)])
-rho = np.array([i['density'] for i in (humid_air_c + humid_air_f)])  # kg/m3
-
-mdotc = mdotc/3600*np.mean(rho[0:3])  # kg/s
-mdotf = mdotf/3600*np.mean(rho[3:])
+    humid_air_f = [HumidAir().with_state(
+        InputHumidAir.altitude(80),
+        InputHumidAir.temperature(i),
+        InputHumidAir.relative_humidity(45)).as_dict() for i in tempf]
+    # print('air froid', humid_air_f)
 
 
-# for idx_w, width in enumerate(w):
-idx_w = 1
-width = w[idx_w]
+    # constantes pour l'air
+    cpf = np.array([i['specific_heat'] for i in (humid_air_f)]).reshape(nb, 3, 1)
+    cpc = np.array([i['specific_heat'] for i in (humid_air_c)]).reshape(nb, 3, 1)
+    cp = np.hstack((cpc, cpf))
+    # mu = np.array([i['dynamic_viscosity'] for i in (humid_air_c + humid_air_f)])
+    kf = np.array([i['conductivity'] for i in (humid_air_f)]).reshape(nb, 3, 1)
+    kc = np.array([i['conductivity'] for i in (humid_air_c)]).reshape(nb, 3, 1)
+    k = np.hstack((kc, kf))
+    # Pr = np.array([i['prandtl'] for i in (humid_air_c + humid_air_f)])
+    rhof = np.array([i['density'] for i in (humid_air_f)]).reshape(nb, 3, 1)  # kg/m3
+    rhoc = np.array([i['density'] for i in (humid_air_c)]).reshape(nb, 3, 1)  # kg/m3
 
-Ac = tcanal*width  # l'air chaud circule dans le sens de la longueur pour un transfert de chaleur maximal
-Af = tcanal*width
-Pc = 2*(tcanal+l)
-Pf = 2*(tcanal+l)
-Dhf = 4*Af/Pf
-Dhc = 4*Ac/Pc
+    mdotc = mdotc/3600*rhoc  # kg/s
+    mdotf = mdotf/3600*rhof  # 51 x 3 x 1
 
-for idx in range(0,3):
-    print('pour Tfi = ', Tfi[idx]-273)
-    hf = k[idx+3]*7.54/Dhf
-    hc = k[idx]*7.54/Dhc
-    U = (1/hc+1/hf+tparoi/kparoi)**-1
-    nparois = 2*nplaques-1
-    Aparoi = nparois*width*l
-    Cf = mdotf*cp[idx+3]
-    Cc = mdotc*cp[idx]
-    NTU = U*Aparoi/min(Cf, Cc)
-    Cr = min(Cf, Cc)/max(Cf, Cc)
+    hf = kf*7.54/Dhf  # 51 x 3 x 1
+    hc = kc*7.54/Dhc  # 51 x 3 x 1
+    U = 1/(1/hc+1/hf+tparoi/kparoi)  # 51 x 3 x 1
+    nparois = 2*nplaques-1  # 51 x 3 x 1
+    Aparoi = nparois*width*l  # 51 x 3 x 1
+    Cf = cpf*mdotf  # 51 x 3 x 1
+    Cc = cpc*mdotc  # 51 x 3 x 1
+    NTU = U*Aparoi/np.minimum(Cf, Cc)  # 51 x 3 x 1
+    Cr = np.minimum(Cf, Cc)/np.maximum(Cf, Cc)  # 51 x 3 x 1
 
-    """if idx_w == 0 :
-        arg = 1/Cr*NTU**.22*(np.exp(-Cr*NTU**.78)-1)
-        Ecroise = 1-np.exp(arg)
-        q = Ecroise*min(Cf, Cc)*(Tci-Tfi[idx])
-        print("q croise", q)"""
-        
-    if idx_w == 1 :
-        num = 1-np.exp(-NTU*(1-Cr))
-        denom = 1-Cr*np.exp(-NTU*(1-Cr))
-        Econtre = num/denom
-        q = Econtre*min(Cf, Cc)*(Tci-Tfi[idx])
-        print("q contre", q)
+    # pour les flux à contre-courants
+    num = 1-np.exp(-NTU*(1-Cr))   # 51 x 3 x 1
+    denom = 1-Cr*np.exp(-NTU*(1-Cr))  # 51 x 3 x 1
+    Econtre = num/denom  # 51 x 3 x 1
+    q = Econtre*np.minimum(Cf, Cc)*(Tci-Tfi)  # 51 x 3 x 1
+    if it == 0:
+        print(q)
+    qmat = q
+    Emat = Econtre
 
-    # Tco = Tci - q / Cc
-    # Tfo = Tfi[idx] + q / Cf
+    Tco = Tci - q / Cc  # 51 x 3 x 1
+    Tfo = Tfi + q / Cf  # 51 x 3 x 1
+    Tcm = (Tci + Tco)/2 - 273.15
+    Tfm = (Tfi + Tfo)/2 - 273.15
+    Tm = np.hstack((Tcm, Tfm))  # 51 x 6 x 1
+    Tmat = np.dstack((Tmat, Tm))  # 51 x 6 x 1+it
 
-    # print("Tco", Tco, 'K ', Tco-273.15, 'C')
-    # print("Tfo", Tfo, 'K ', Tfo-273.15, 'C')
 
-    # print('Tcm', (Tci + Tco)/2 -273.15)
-    # print('Tfm', (Tfi[idx] + Tfo)/2 -273.15)
+for i in range(3):
+    mpl.plot(nplaques[:,0,0], Emat[:,i,0], "-", label=f'Tfi = {int(Tfi[0,i,0]-273.15)} \u2103')
 
-    mpl.plot(nplaques, q, "-", label=f'Tfi = {int(Tfi[idx]-273)} \u2103')
+    # on va choisir une efficacité minimale de 90%, qui est atteint avec plus de plaques pour les hautes températures
 
 mpl.legend()
 mpl.grid()
